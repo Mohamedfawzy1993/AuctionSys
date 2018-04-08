6a84707f8b7070e946ff6719fe3e82d6cb9e988d
Mohamed.Fawzy <mohamedfawzy1993@gmail.com>
2018-04-08 15:58:52 +0200

    Regiser Auction Start'

--- AuctionSite/AuctionSite-web/src/main/java/web/RegisterAuctionBean.java
+++ AuctionSite/AuctionSite-web/src/main/java/web/RegisterAuctionBean.java
@@ -3,36 +3,26 @@ package web;
 
 import model.dto.Auction;
 import model.dto.Product;
+import model.dto.UserSellProduct;
 
 import javax.enterprise.context.RequestScoped;
-import javax.faces.model.ArrayDataModel;
-import javax.faces.model.CollectionDataModel;
-import javax.faces.model.DataModel;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Named(value = "registerAuctionBean")
 @RequestScoped
 public class RegisterAuctionBean implements Serializable{
 
-    private List<Product> productList;
     private Auction auction;
-    private DataModel<Product> products;
+    private List<Product> products;
+    private List<UserSellProduct> userSellProducts;
+
     public RegisterAuctionBean() {
-        auction = new Auction();
-        productList = new ArrayList<>();
-        products = new CollectionDataModel<>(productList);
-    }
-
-
-    public List<Product> getProductList() {
-        return productList;
-    }
-
-    public void setProductList(List<Product> productList) {
-        this.productList = productList;
+        products = new ArrayList<>();
+        userSellProducts = new ArrayList<>();
     }
 
     public Auction getAuction() {
@@ -45,8 +35,6 @@ public class RegisterAuctionBean implements Serializable{
 
     public void printIT()
     {
-
         System.out.println(auction.getAuctionDesc());
-
     }
 }
